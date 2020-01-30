0d63e93681712cec2d31f617e2d29d8ebe75856a
Brandon Franks <bgfranks106@icloud.com>
2020-01-29 22:05:34 -0500

    finsished mvp and added delete cart function

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
 
+// Contexts
+import ProductContext from "./context/productContext";
+import CartContext from "./context/cartContext";
+
 function App() {
   const [products] = useState(data);
   const [cart, setCart] = useState([]);
@@ -16,18 +20,20 @@ function App() {
     setCart(cart => [...cart, item]);
   };
 
+  const removeItem = removeThis => {
+    setCart(cart => cart.filter(item => item.id !== removeThis));
+  };
+
   return (
     <div className="App">
-      <Navigation cart={cart} />
-
-      {/* Routes */}
-      <Route
-        exact
-        path="/"
-        render={() => <Products products={products} addItem={addItem} />}
-      />
-
-      <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+      <ProductContext.Provider value={{ products, addItem }}>
+        <CartContext.Provider value={{ cart, removeItem }}>
+          <Navigation cart={cart} />
+          {/* Routes */}
+          <Route exact path="/" component={Products} />
+          <Route path="/cart" component={ShoppingCart} />
+        </CartContext.Provider>
+      </ProductContext.Provider>
     </div>
   );
 }
